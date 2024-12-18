b8c958e7732cfb47c1caf7c778d7d667433f5f95
Rahmadjon0038 <abdullayevrahmadjon@gmail.com>
2024-12-18 10:49:59 +0500

    pagination

--- src/root/RootControl.jsx
+++ src/root/RootControl.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Home from '../components/home/Home'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { rootdata } from '../utils/RootData'
+import ArrowButtons from '../components/home/ArrowButtons'
 
 function RootControl() {
   const {pathname} = useLocation()
@@ -16,6 +17,7 @@ function RootControl() {
           <Route key={item.id} path={item.path} element={item.name} />
         ))}
       </Routes>
+
      
     </div>
   )
